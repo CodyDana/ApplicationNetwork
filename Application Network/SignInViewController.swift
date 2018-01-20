2ccf1c45bb815729a2cfda2977f8e08712b1a95d
Cody Dana <codydana415@gmail.com>
2018-01-20 14:27:37 -0700

    Added Login/Registeration Pages.

---
+++ Application Network/SignInViewController.swift
@@ -0,0 +1,39 @@
+//
+//  SignInViewController.swift
+//  Application Network
+//
+//  Created by Cody Dana on 1/20/18.
+//  Copyright © 2018 Cody Dana. All rights reserved.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func signInButtonTapped(_ sender: Any) {
+        // 1. Sign in user
+        // 2. If username and password are correct, take user to protected page (tab bar application).
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
